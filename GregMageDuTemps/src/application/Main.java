81a8aec0679eff0ca9be28f259a7e5a0d2afcc29
Gregory-Nam <gregory.nam@orange.fr>
2020-01-08 18:33:28 +0100

    creation projet javafx

---
+++ GregMageDuTemps/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//Panneau qui correspond a la vue "UneFenetre.fxml"
+			AnchorPane root = FXMLLoader.load(getClass().getResource("/Vue/UneFenetre.fxml"));
+			//On assoscie à la scene le panneau cree precedemment
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
