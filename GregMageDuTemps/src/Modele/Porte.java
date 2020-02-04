fa3398a9116271f7ecf0d6c40b184738f9b02130
Hugo <55799941+BOBYBEL@users.noreply.github.com>
2020-02-04 10:44:04 +0100

    Création du package utilitaire

--- GregMageDuTemps/src/Modele/Porte.java
+++ GregMageDuTemps/src/Modele/Porte.java
@@ -1,7 +1,19 @@
 package Modele;
 
+import utilitaire.SalleBoolean;
+
 public class Porte extends Interactif{
 
+	
+	private SalleBoolean tab [];
+	
+	
+	public Porte(Salle salleBefore, Salle salleAfter) {
+		tab = new SalleBoolean [1];
+		tab [0] = new SalleBoolean(salleBefore, false);
+		tab [1] = new SalleBoolean(salleAfter, false);
+	}
+	
 	@Override
 	public void interagir() {
 		// TODO Auto-generated method stub
