9bbd94b7ae30a7b647a17094494baa7d1784f226
Gregory-Nam <gregory.nam@orange.fr>
2020-02-27 12:58:25 +0100

    ajout des tostring

--- GregMageDuTemps/src/enumerations/NomPNJ.java
+++ GregMageDuTemps/src/enumerations/NomPNJ.java
@@ -1,5 +1,17 @@
 package enumerations;
 
 public enum NomPNJ {
-	TEST, NOMAUPIF;
+	KLACE("Klace"), NOM_PNJ_2("NOM_PNJ2"), NOM_PNJ_3("NOM_PNJ3"), NOM_PNJ_4("NOM_PNJ4"), NOM_PNJ_5("NOM_PNJ5");
+	
+	private final String nom;
+	
+	private NomPNJ(String s) {
+		this.nom = s;
+	}
+	
+	@Override
+	public String toString() {
+		return nom;
+	}
+	
 }
