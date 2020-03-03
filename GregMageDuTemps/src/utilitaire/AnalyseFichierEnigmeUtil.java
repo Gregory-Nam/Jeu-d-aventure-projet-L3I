caf27f4ffd396d42559d8f90a129a879661d1e1f
ABMBAYE <55805169+ABMBAYE@users.noreply.github.com>
2020-03-04 00:25:10 +0100

    Le javadoc de la classe AFEU

--- GregMageDuTemps/src/utilitaire/AnalyseFichierEnigmeUtil.java
+++ GregMageDuTemps/src/utilitaire/AnalyseFichierEnigmeUtil.java
@@ -6,12 +6,29 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import enumerations.TypeDialogue;
+/**
+ * 
+ *@author Ahmadou Bamba MBAYE
+ *Cette classe permet d'analyser le fichier contenant les enigmes
+ */
 public final class AnalyseFichierEnigmeUtil {
 
 	
 	/* CLASSE UTILITAIRE, ON VEUT PAS QU'ELLE SOIT INSTANCIEE */
+	/**
+	 * CLASSE UTILITAIRE, ON VEUT PAS QU'ELLE SOIT INSTANCIEE
+ 	*/
 	private AnalyseFichierEnigmeUtil() {}
-	
+	/**
+	 * Cette methode contient deux parametres le nom du personnage non joueur et un type de dialogue.
+	 * Elle crée une nouvelle variable de type JSONParse qui va contenant le JSONParse()
+	 * @param nomPNJ
+	 * Le nom du personnage non joueur
+	 * @param type
+	 * Le type de dialogue
+	 * @return
+	 * Elle retourne null
+	 */
 	public static String initDialoguesJSON(String nomPNJ, TypeDialogue type)  {
 		JSONParser parser = new JSONParser();
 		System.out.println(nomPNJ);
