8b3c76e42f4332ab879aa740d3548103206fc8ed
ABMBAYE <55805169+ABMBAYE@users.noreply.github.com>
2020-03-03 16:13:57 +0100

    Le javadoc de l'enum Materiaux

--- GregMageDuTemps/src/enumerations/Materiaux.java
+++ GregMageDuTemps/src/enumerations/Materiaux.java
@@ -1,5 +1,21 @@
 package enumerations;
-
+/**
+ *
+ * @author Ahmadou Bamba MBAYE
+ * Cette ennumération contient l'ensemble des items que le joueur peut
+ * gagner dans cette ordre :
+ * <ol>
+ *     <li>BRONZE 3eme dans l'hierarchie des items</li>
+ *   
+ *     <li>ARGENT 2eme dans l'hierarchie des items</li>
+ *    
+ *     <li>OR 1er dans l'hierarchie des items</li>
+ *
+ *	<li>PLAQUE_OR</li>
+ *     
+ * </ol>
+ *
+ */
 public enum Materiaux {
 
 	BRONZE, ARGENT, OR, PLAQUE_OR;
