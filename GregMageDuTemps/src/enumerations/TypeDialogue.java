d7d91f9e7c43b90c2f20648a011e284d5234d8d6
ABMBAYE <55805169+ABMBAYE@users.noreply.github.com>
2020-03-03 16:29:44 +0100

    Le javadoc de l'enum TypeDialogue

--- GregMageDuTemps/src/enumerations/TypeDialogue.java
+++ GregMageDuTemps/src/enumerations/TypeDialogue.java
@@ -1,5 +1,16 @@
 package enumerations;
-
+/**
+ *
+ * @author Ahmadou Bamba MBAYE<br>
+ * Cette ennumération contient la liste du dialogue:
+ *<ul>
+ *     <li>QUESTION : la question posee</li>
+ *     <li>BONNE_REPONSE : la bonne reponse</li>
+ *     <li>MAUVAISE_REPONSE : la mauvaise reponse</li>
+ *     <li>DEJA_REPONDU : si la question est deja repondu</li>
+ *     <li>REPONSE : la reponse de la question posee</li>
+ *</ul>
+ */
 public enum TypeDialogue {
 	QUESTION, BONNE_REPONSE, MAUVAISE_REPONSE, DEJA_REPONDU, REPONSE;
 }
