e441d8ff193ed28f4bfc6627fb3efcb1e20eba09
ABMBAYE <55805169+ABMBAYE@users.noreply.github.com>
2020-03-03 16:12:06 +0100

    Le javadoc de l'enum du Déplacements

--- GregMageDuTemps/src/enumerations/Deplacements.java
+++ GregMageDuTemps/src/enumerations/Deplacements.java
@@ -1,11 +1,28 @@
 package enumerations;
 import javafx.scene.input.KeyCode;
+/**
+ *
+ * @author Ahmadou Bamba MBAYE.
+ *Cette ennumeration contient les elements suivants :
+ *<ul>
+	 *<li>HAUT quand on presse le bouton vers le haut</li>
+	 *<li>DROITE quand on presse le bouton vers la droite </li>
+	 *<li>BAS quand on presse le bouton vers le bas</li>
+	 *<li>GAUCHE quand on presse le bouton vers la gauche </li>
+ *</ul>
+ */
 public enum Deplacements {
 	
 	HAUT(KeyCode.UP), DROITE(KeyCode.RIGHT), BAS(KeyCode.DOWN), GAUCHE(KeyCode.LEFT);
-	
+	/**
+	 * La clee du code
+	 */
 	final private KeyCode kc;
-	
+	/**
+	 * Le constructeur permet: 
+	 * L'initialisation de la clee
+	 * @param kc La clee du code
+	 */
 	private Deplacements(KeyCode kc) {
 		this.kc = kc;
 	}
