2950c52accd3454aa833548c0363491c72d2e150
ABMBAYE <55805169+ABMBAYE@users.noreply.github.com>
2020-03-03 23:41:41 +0100

    Le javadoc de la classe Personnage

--- GregMageDuTemps/src/personnages/Personnage.java
+++ GregMageDuTemps/src/personnages/Personnage.java
@@ -8,21 +8,49 @@ import elements.Item;
 import enumerations.Deplacements;
 import javafx.scene.image.*;
 import java.io.File;
-
+/**
+ *
+ * @author Ahmadou Bamba MBAYE.
+ * Cette classe abstraite nommee personnage herite la classe Interactif.Elle gere les personnages joueur et les personnages non joueur
+ * Elle gere le deplacement des personnages avec des methodes seDirigerADroite() quand le personnage se dirige vers la droite
+ * et seDirigerAGauche() vers la gauche
+ *
+ *
+ */
 public abstract class Personnage extends Interactif {
+	/**
+	 * Le HashMap contient l'ensemble des des sprites pour le personnage..
+	 *
+	 */
 	protected HashMap<Deplacements, ImageView> spritesPersonnageHM;
+	/**
+	 * La vue de l'image courante du personnage en faisant une paire (imageview, Deplacement)
+	 */
 	protected ImageView spriteCourant;
+	/**
+	*Litem étant en possession
+	*/
 	Item itemEnPossession;
-	
+	/**
+	 * Le constructeur de la classe personnage cree un sprite personnage
+	 * @param sprites Les sprites
+	 */
 	public Personnage () {
 		spritesPersonnageHM = new HashMap<Deplacements,ImageView>();
 	}
-	
+	/**
+	 * La méthode abstraite seDirigerADroite() gére la sas ou le personnage se dirige vers la droite..
+	 */
 	public abstract void seDirigerADroite();
-	
+	/**
+	 * La méthode abstraite seDirigerAGauche() gére la sas ou le personnage se dirige vers la gauche..
+	 */
 	public abstract void seDirigerAGauche();
 	
-	
+	/**
+	 * La methode initPersonnage() gere les sprites du personnage et les images associees.
+	 * @param sprites Les sprites
+	 */
 	public void initPersonnage(File sprites[]) {
 		Deplacements[] deplacements = Deplacements.values();
 		for(int i = 0; i < sprites.length; ++i) {
@@ -32,7 +60,10 @@ public abstract class Personnage extends Interactif {
 			spritesPersonnageHM.put(deplacements[i], iv);
 		}
 	}
-	
+	/**
+	 * La methode abstraite getImageView() recupere l'image en vue
+	 * @return Elle retourne le sprint courant
+	 */
 	public ImageView getImageView() {
 		return spriteCourant;
 	}
