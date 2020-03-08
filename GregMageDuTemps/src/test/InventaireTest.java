46bc1fe86c68516fd76a999e37e25803fec88603
BOBYBEL <55799941+BOBYBEL@users.noreply.github.com>
2020-03-08 12:19:35 +0100

    Ajout des méthodes de test dans inventaire, arrêt des tests unitaires sur JUnit car ils ne fonctionnent pas

--- GregMageDuTemps/src/test/InventaireTest.java
+++ GregMageDuTemps/src/test/InventaireTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
@@ -10,16 +11,22 @@ import org.junit.jupiter.api.Test;
 
 import elements.Inventaire;
 import elements.Item;
+import enumerations.Materiaux;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 class InventaireTest {
 
 	Inventaire inv;
+	Item testItem;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		inv = new Inventaire();
+		testItem = new Item(new File("Images/items/aiguille_bronze_transparence.png"),
+			       new File("Images/items/aiguille_bronze.png"),
+			       Materiaux.BRONZE, 630, "Aiguille");
+		inv.ajouterItem(testItem);
 	}
 
 	@AfterEach
@@ -28,7 +35,7 @@ class InventaireTest {
 
 	@Test
 	void testInventaire() {
-		
+		fail("Not yet implemented");
 	}
 
 	@Test
@@ -38,18 +45,24 @@ class InventaireTest {
 
 	@Test
 	void testSupprimerItem() {
-		fail("Not yet implemented");
+		inv.supprimerItem(testItem);
+		assertNull(inv.getItem(testItem));
 	}
 
 	@Test
-	void testGetItem() {
-		fail("Not yet implemented");
+	void testGetItem() {	
+		assertEquals(testItem, inv.getItem(testItem));
 	}
 
 	@Test
 	void testGetInventaire() {
 		ObservableList<Item> ol = FXCollections.observableArrayList(new ArrayList<Item>());
 		assertEquals(ol, inv.getInventaire());
+	}
+	
+	@Test
+	void testCapaciteAtteinte() {
+		fail("Not yet implemented");
 	}
 
 	@Test
