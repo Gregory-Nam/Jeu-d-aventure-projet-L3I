a39b20e784e17ffc773e211deae098bf57cdc68a
BOBYBEL <55799941+BOBYBEL@users.noreply.github.com>
2020-03-24 16:09:35 +0100

    Création des classes de test des personnages joueurs et non joueurs

---
+++ GregMageDuTemps/src/test/PersonnageJoueurTest.java
@@ -0,0 +1,114 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonnageJoueurTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testInteragir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetXMin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetXMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetXCentre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSeDirigerADroite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSeDirigerAGauche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInitPersonnage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testChangerSprite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEnleverItemEnMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReplacerGauche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReplacerDroite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrendreItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrendreItemEnMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetItemEnMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInstanceUnique() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInventaire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testABienRepondu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReinitialiser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLiaisonDialogueAvecPNJ() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ GregMageDuTemps/src/test/PersonnageNonJoueurTest.java
@@ -0,0 +1,99 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonnageNonJoueurTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testInteragir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetXMin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetXMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSeDirigerADroite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSeDirigerAGauche() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPersonnageNonJoueur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPoseQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRepondAUneMauvaiseReponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRepondAUneBonneReponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDitQueTuAsDejaRepondu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEtatReponseAttendu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDonnerItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetImagePourEnigme() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testARecuUneBonneReponse() {
+		fail("Not yet implemented");
+	}
+
+}
